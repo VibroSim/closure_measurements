Correct sign in TestRegistration; rename parameters so they fit better into experiment logs if needed. Tighten up default tiptolerance for processtrak step because processtrak typically used with registration process that minimizes lateral errors.

closure_measurements/process_dic.py
@@ -140,8 +140,8 @@ def TestRegistration(nloads,Xposvecs,u_disps,v_disps,
                 v_values = v_disps[:,:,idx1,idx2,XCnt][ROI_out_arrays[:,:,idx1,idx2,XCnt].astype(np.bool)] # only use values where ROI==1
                 meanv = np.mean(v_values.ravel());
 
-                predu = relshift_firstimg_lowerleft_corner_x_diff[idx1,idx2]-relshift_firstimg_lowerleft_corner_x_ref[idx1,idx2]
-                predv = relshift_firstimg_lowerleft_corner_y_diff[idx1,idx2]-relshift_firstimg_lowerleft_corner_y_ref[idx1,idx2]
+                predu = -(relshift_firstimg_lowerleft_corner_x_diff[idx1,idx2]-relshift_firstimg_lowerleft_corner_x_ref[idx1,idx2])
+                predv = -(relshift_firstimg_lowerleft_corner_y_diff[idx1,idx2]-relshift_firstimg_lowerleft_corner_y_ref[idx1,idx2])
                 
                 print("idx1 = %d; idx2 = %d; XCnt = %d; meanu = %f um; predu = %f um; meanv= %f um; predv=%f um" % (idx1,idx2,XCnt,meanu*1e6,predu*1e6,meanv*1e6,predv*1e6))
                 pass