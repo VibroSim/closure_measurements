2fe207af79a697a4740cac4be175e4a8fa102040
Steve Holland <sdh4@iastate.edu>
2019-12-12 11:35:03 -0600

    Python distutils/setuptools uses "unix" to refer to gcc

--- setup.py
+++ setup.py
@@ -6,11 +6,65 @@ import subprocess
 from setuptools import setup
 from setuptools.command.install_lib import install_lib
 from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
 import setuptools.command.bdist_egg
 import sys
 #import glob
 
 from Cython.Build import cythonize
+
+
+extra_compile_args = {
+    "msvc": ["/std:c++14"],
+    #"unix": ["-O0", "-g", "-Wno-uninitialized"),    # Replace the line below with this line to enable debugging of the compiled extension
+    "unix": ["-O5","-Wno-uninitialized","-std=c++11"],
+    #"unix": ["-O0","-Wno-uninitialized","-std=c++11","-g"],
+    "clang": ["-O5","-Wno-uninitialized"],
+}
+
+extra_include_dirs = {
+    "msvc": ["c:\\suitesparse", np.get_include() ],
+    "unix": ["/usr/include/suitesparse",np.get_include() ],
+    "clang": ["/usr/include/suitesparse", np.get_include() ],
+}
+
+extra_libraries = {
+    "msvc": ["ncorr","opencv_core","opencv_imgproc","opencv_videoio","opecv_highgui","cholmod","spqr","fftw3"],
+    "unix": ["ncorr","opencv_core","opencv_imgproc","opencv_videoio","opecv_highgui","cholmod","spqr","fftw3"],
+    "clang": ["ncorr","opencv_core","opencv_imgproc","opencv_videoio","opecv_highgui","cholmod","spqr","fftw3"],
+}
+
+extra_link_args = {
+    "msvc": [],
+    "unix": [],
+    "clang": [],
+}
+
+class build_ext_compile_args(build_ext):
+    def build_extensions(self):
+        compiler=self.compiler.compiler_type
+        for ext in self.extensions:
+            if compiler in extra_compile_args:
+                ext.extra_compile_args=extra_compile_args[compiler]
+                ext.extra_link_args=extra_link_args[compiler]
+                ext.include_dirs.extend(list(extra_include_dirs[compiler]))
+                ext.libraries.extend(list(extra_libraries[compiler]))
+                pass
+            else:
+                # use unix parameters as default
+                ext.extra_compile_args=extra_compile_args["unix"]
+                ext.extra_link_args=extra_link_args["unix"]
+                ext.include_dirs.extend(list(extra_include_dirs["unix"]))
+                ext.libraries.extend(extra_libraries["unix"])
+                pass
+                
+            pass
+            
+        
+        build_ext.build_extensions(self)
+        pass
+    pass
+
 
 class install_lib_save_version(install_lib):
     """Save version information"""
@@ -80,14 +134,14 @@ ext_modules=cythonize("closure_measurements/*.pyx",language="c++")
 
 emdict=dict([ (module.name,module) for module in ext_modules])
 
-correlate_pyx_ext=emdict['closure_measurements.correlate']
-#correlate_pyx_ext.sources.append("extra_c_file.c")
-correlate_pyx_ext.extra_compile_args=['-std=c++11', '-O0','-g']
-correlate_pyx_ext.include_dirs = ['/usr/include/suitesparse']
-correlate_pyx_ext.libraries = ['ncorr','opencv_core','opencv_imgproc','opencv_videoio','opencv_highgui','cholmod','spqr','fftw3']
+#correlate_pyx_ext=emdict['closure_measurements.correlate']
+##correlate_pyx_ext.sources.append("extra_c_file.c")
+#correlate_pyx_ext.extra_compile_args=['-std=c++11', '-O0','-g']
+#correlate_pyx_ext.include_dirs = ['/usr/include/suitesparse']
+#correlate_pyx_ext.libraries = ['ncorr','opencv_core','opencv_imgproc','opencv_videoio','opencv_highgui','cholmod','spqr','fftw3']
 
-#correlate_pyx_ext.extra_compile_args=['-fopenmp','-O3']
-#correlate_pyx_ext.extra_link_args=['-lgomp']
+##correlate_pyx_ext.extra_compile_args=['-fopenmp','-O3']
+##correlate_pyx_ext.extra_link_args=['-lgomp']
 
 
 
@@ -99,7 +153,8 @@ setup(name="closure_measurements",
       zip_safe=False,
       packages=["closure_measurements",
                 "closure_measurements.bin"],
-      cmdclass={"install_lib": install_lib_save_version },
+      cmdclass={"install_lib": install_lib_save_version,
+                "build_ext": build_ext_compile_args},
       #data_files=[ ("share/closure_measurements/pt_steps",pt_steps_files),]
       package_data={"closure_measurements": closure_measurements_package_files},
       entry_points={ "limatix.processtrak.step_url_search_path": [ "limatix.share.pt_steps = closure_measurements:getstepurlpath" ],
